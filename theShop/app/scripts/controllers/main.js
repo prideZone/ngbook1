6a463eabd5f6bc21d557da5e701ae1e323ce1d3d
KumarGauravMishra <kumar.mishra@firstcry.com>
2017-03-01 13:12:18 +0530

    Basic Setup completed

---
+++ theShop/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theShopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the theShopApp
+ */
+angular.module('theShopApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
